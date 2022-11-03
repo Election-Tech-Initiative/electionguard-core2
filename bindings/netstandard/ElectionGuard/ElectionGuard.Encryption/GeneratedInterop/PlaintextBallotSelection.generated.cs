4e28f42bfbbcbdab0e827486cb4a91a19820cbc1
Lee Richardson <github@leerichardson.com>
2022-11-03 16:26:51 -0400

    This is how it was and how it works

--- bindings/netstandard/ElectionGuard/ElectionGuard.Encryption/GeneratedInterop/PlaintextBallotSelection.generated.cs
+++ bindings/netstandard/ElectionGuard/ElectionGuard.Encryption/GeneratedInterop/PlaintextBallotSelection.generated.cs
@@ -85,7 +85,7 @@ namespace ElectionGuard
             {
                 protected override bool Free()
                 {
-                    if (IsFreed) return true;
+                    if (IsClosed) return true;
 
                     var status = External.Free(TypedPtr);
                     if (status != Status.ELECTIONGUARD_STATUS_SUCCESS)
