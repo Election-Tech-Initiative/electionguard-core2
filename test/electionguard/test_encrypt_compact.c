defb35c1d6dc921c466ef620b4d80e4ad506e592
Lee Richardson <github@leerichardson.com>
2022-11-07 08:00:54 -0500

    fix build error

--- test/electionguard/test_encrypt_compact.c
+++ test/electionguard/test_encrypt_compact.c
@@ -6,6 +6,7 @@
 #include <assert.h>
 #include <electionguard/encrypt.h>
 #include <electionguard/ciphertext_ballot_selection.generated.h>
+#include <electionguard/plaintext_ballot.generated.h>
 #include <stdlib.h>
 
 bool strings_are_equal(char *expected, char *actual);
