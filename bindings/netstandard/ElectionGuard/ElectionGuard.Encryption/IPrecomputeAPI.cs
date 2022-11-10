d202d042e774ebf0cd4f7b19549bb13a043199a4
Lee Richardson <github@leerichardson.com>
2022-11-10 15:50:03 -0500

    cleanup new linting errors

--- bindings/netstandard/ElectionGuard/ElectionGuard.Encryption/IPrecomputeAPI.cs
+++ bindings/netstandard/ElectionGuard/ElectionGuard.Encryption/IPrecomputeAPI.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Threading;
-
-namespace ElectionGuard
+﻿namespace ElectionGuard
 {
     /// <summary>
     /// Interface for the precompute API to start and stop the precompute calculations
--- bindings/netstandard/ElectionGuard/ElectionGuard.Encryption/Precompute.cs
+++ bindings/netstandard/ElectionGuard/ElectionGuard.Encryption/Precompute.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 // ReSharper disable RedundantAssignment
 // ReSharper disable InlineOutVariableDeclaration
 
