b9743eb50eb7438444cadcdf7462aaa6e494fe9b
John Morgan <70619927+john-s-morgan@users.noreply.github.com>
2022-11-17 11:18:40 -0500

    ElectionGuardUI initial commit (#52)
    * ElectionGuardUI initial commit

---
+++ src/electionguard-ui/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace ElectionGuard.UI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+
+	protected override Window CreateWindow(IActivationState activationState)
+	{
+		var window = base.CreateWindow(activationState);
+		
+		if (string.IsNullOrEmpty(window.Title))
+		{
+			window.Title = GetWindowTitle();
+		}
+
+		return window;
+	}
+
+	// Hack: get this from a resource
+	private string GetWindowTitle() => "ElectionGuard Election Manager";
+}
