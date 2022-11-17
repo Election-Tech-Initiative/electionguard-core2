7fe392016f0ba7d8b37ba3cb8ac487f0b9272da6
John Morgan <john.morgan@infernored.com>
2022-11-17 09:07:12 -0500

    Add app title

--- src/electionguard-ui/App.xaml.cs
+++ src/electionguard-ui/App.xaml.cs
@@ -8,4 +8,19 @@ public partial class App : Application
 
 		MainPage = new AppShell();
 	}
+
+	protected override Window CreateWindow(IActivationState activationState)
+	{
+		var window = base.CreateWindow(activationState);
+		
+		if (string.IsNullOrEmpty(window.Title))
+		{
+			window.Title = GetWindowTitle();
+		}
+
+		return window;
+	}
+
+	// Hack: get this from a resource
+	private string GetWindowTitle() => "ElectionGuard Election Manager";
 }
