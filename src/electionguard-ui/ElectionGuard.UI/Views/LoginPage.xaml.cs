b8c6511b73d9edf8c8ec73c87da50f04b506ea6c
Lee Richardson <github@leerichardson.com>
2022-11-28 12:05:00 -0500

    On enter log in on login page

--- src/electionguard-ui/ElectionGuard.UI/Views/LoginPage.xaml.cs
+++ src/electionguard-ui/ElectionGuard.UI/Views/LoginPage.xaml.cs
@@ -1,12 +1,22 @@
+using Windows.UI.Core;
+
 namespace ElectionGuard.UI.Views;
 
-public partial class LoginPage : ContentPage
+public partial class LoginPage
 {
 	public LoginPage(LoginViewModel vm)
 	{
 		InitializeComponent();
 		BindingContext = vm;
 		vm.Page = this;
-	}
+        CoreWindow.GetForCurrentThread().KeyDown += LoginPage_KeyDown;
+    }
 
+    private void LoginPage_KeyDown(CoreWindow sender, KeyEventArgs args)
+    {
+        if (args.VirtualKey == Windows.System.VirtualKey.Enter)
+        {
+            LoginButton.SendClicked();
+        }
+    }
 }
