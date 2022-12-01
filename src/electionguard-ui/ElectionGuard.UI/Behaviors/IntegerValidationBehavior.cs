c764bab6e7e43351649fb9f6a8f8a163f2519df3
John Morgan <john.morgan@infernored.com>
2022-12-01 10:44:54 -0500

    Update on PR feedback

--- src/electionguard-ui/ElectionGuard.UI/Behaviors/IntegerValidationBehavior.cs
+++ src/electionguard-ui/ElectionGuard.UI/Behaviors/IntegerValidationBehavior.cs
@@ -1,15 +1,15 @@
-namespace  ElectionGuard.UI.Behaviors;
+namespace ElectionGuard.UI.Behaviors;
 
 public partial class IntegerValidationBehavior : Behavior<Entry>
 {
     public static readonly BindableProperty AttachBehaviorProperty =
         BindableProperty.CreateAttached("AttachBehavior", typeof(bool), typeof(IntegerValidationBehavior), false, propertyChanged: OnAttachBehaviorChanged);
-    
+
     public static bool GetAttachBehavior(BindableObject view)
     {
         return (bool)view.GetValue(AttachBehaviorProperty);
     }
-    
+
     public static void SetAttachBehavior(BindableObject view, bool value)
     {
         view.SetValue(AttachBehaviorProperty, value);
@@ -41,7 +41,7 @@ public partial class IntegerValidationBehavior : Behavior<Entry>
             return;
         }
 
-        if (!args.NewTextValue.ToCharArray().All(char.IsDigit))
+        if (!args.NewTextValue.All(char.IsDigit))
         {
             ((Entry)sender).Text = args.OldTextValue;
         }
