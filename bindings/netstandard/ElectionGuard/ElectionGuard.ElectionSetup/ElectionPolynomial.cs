8c9a01f9e6f4a0767ea40ee6b794c9891549e8f0
Lee Richardson <github@leerichardson.com>
2022-12-01 16:47:30 -0500

    Port a little more GeneratePolynomial code

--- bindings/netstandard/ElectionGuard/ElectionGuard.ElectionSetup/ElectionPolynomial.cs
+++ bindings/netstandard/ElectionGuard/ElectionGuard.ElectionSetup/ElectionPolynomial.cs
@@ -13,6 +13,11 @@ namespace ElectionGuard.ElectionSetup
     /// </summary>
     public class ElectionPolynomial
     {
+        public ElectionPolynomial(List<Coefficient> coefficients)
+        {
+            Coefficients = coefficients;
+        }
+
         public List<Coefficient> Coefficients;
 
         /// <summary>
@@ -43,30 +48,30 @@ namespace ElectionGuard.ElectionSetup
         /// <returns>Polynomial used to share election keys</returns>
         public static ElectionPolynomial GeneratePolynomial(int numberOfCoefficients, ElementModQ nonce)
         {
-            // todo: the hard stuff in C++ goes here
-            throw new NotImplementedException();
+            List<Coefficient> coefficients = new List<Coefficient>();
 
-            //List<Coefficient> coefficients = new List<Coefficient>();
+            for (int i = 0; i < numberOfCoefficients; i++)
+            {
+                // todo: the hard stuff in C++ goes here
+                throw new NotImplementedException();
 
-            //for (int i = 0; i < numberOfCoefficients; i++)
-            //{
-            //    // Note: the nonce value is not safe. It is designed for testing only.
-            //    // This method should be called without the nonce in production.
-            //    ElementModQ value;
-            //    if (nonce != null)
-            //        value = add_q(nonce, i);
-            //    else
-            //        value = rand_q();
+                //    // Note: the nonce value is not safe. It is designed for testing only.
+                //    // This method should be called without the nonce in production.
+                //    ElementModQ value;
+                //    if (nonce != null)
+                //        value = add_q(nonce, i);
+                //    else
+                //        value = rand_q();
 
-            //    commitment = g_pow_p(value)
-            //    proof = make_schnorr_proof(
-            //        ElGamalKeyPair(value, commitment), rand_q()
-            //    )  # TODO Alternate schnoor proof method that doesn't need KeyPair
-            //        coefficient = Coefficient(value, commitment, proof)
-            //    coefficients.append(coefficient)
-            //}
+                //    var commitment = g_pow_p(value)
+                //    var proof = make_schnorr_proof(
+                //        ElGamalKeyPair(value, commitment), rand_q()
+                //    )  # TODO Alternate schnoor proof method that doesn't need KeyPair
+                //    var coefficient = new Coefficient(value, commitment, proof)
+                //    coefficients.append(coefficient)
+            }
 
-            //return new ElectionPolynomial(coefficients);
+            return new ElectionPolynomial(coefficients);
         }
     }
 }
