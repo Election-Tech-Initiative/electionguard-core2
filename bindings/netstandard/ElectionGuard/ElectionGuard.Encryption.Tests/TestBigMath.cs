569751915e8849405ee90dc4f78259e2d0fb7691
Lee Richardson <github@leerichardson.com>
2022-12-02 16:07:40 -0500

    Expose rand_q

---
+++ bindings/netstandard/ElectionGuard/ElectionGuard.Encryption.Tests/TestBigMath.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace ElectionGuard.Encryption.Tests
+{
+    [TestFixture]
+    internal class TestBigMath
+    {
+        [Test]
+        public void RandQIsRandom()
+        {
+            var elementModQ1 = BigMath.RandQ();
+            var elementModQ2 = BigMath.RandQ();
+            Assert.IsNotNull(elementModQ1);
+            Assert.AreNotEqual(elementModQ1.ToHex(), elementModQ2.ToHex());
+        }
+    }
+}
---
+++ bindings/netstandard/ElectionGuard/ElectionGuard.Encryption/BigMath.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace ElectionGuard
+{
+    /// <summary>
+    /// Methods related to numbers larger than 64 bits
+    /// </summary>
+    public static class BigMath
+    {
+        /// <summary>
+        /// Generate random number between 0 and Q.
+        /// </summary>
+        public static ElementModQ RandQ()
+        {
+            var status = External.RandQ(
+                out NativeInterface.ElementModQ.ElementModQHandle value);
+            status.ThrowIfError();
+            return new ElementModQ(value);
+        }
+
+        internal static class External
+        {
+            [DllImport(
+                NativeInterface.DllName,
+                EntryPoint = "eg_element_mod_q_rand_q_new",
+                CallingConvention = CallingConvention.Cdecl,
+                SetLastError = true)]
+            internal static extern Status RandQ(out NativeInterface.ElementModQ.ElementModQHandle handle);
+        }
+    }
+}
