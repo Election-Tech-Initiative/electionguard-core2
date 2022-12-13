11f5329932d64080ec57db285ddefea9092e007b
Steve Maier <82616727+SteveMaier-IRT@users.noreply.github.com>
2022-12-13 14:13:51 -0500

    Moved ViewModels back to UI (#97)
    SEtup UI project to support testing
    Add UI project to testing
    Cleanup mac/win compiling differently.

---
+++ src/electionguard-ui/ElectionGuard.UI/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ElectionGuard.UI.ViewModels;
+
+public class SettingsViewModel : BaseViewModel
+{
+    public SettingsViewModel(IServiceProvider serviceProvider) : base(null, serviceProvider)
+    {
+    }
+}
