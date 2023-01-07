747fa8f6b3f12960e3923806468bd8d6421583a5
AddressXception <github@addressxception.com>
2023-01-07 01:59:09 -0500

    reimplement bignum facade

--- src/electionguard/facades/bignum4096.cpp
+++ src/electionguard/facades/bignum4096.cpp
@@ -37,9 +37,6 @@ namespace electionguard::facades
         HaclBignumType element;
         bool prefer32BitMath = false;
 
-        // Impl(uint32_t *elem) { this->prefer32BitMath = true; }
-        // Impl(uint64_t *elem) { this->prefer32BitMath = true; }
-
         Impl(uint32_t *elem) : element(make_unique<hacl::Bignum4096_32>(move(elem)))
         {
             this->prefer32BitMath = true;
@@ -68,14 +65,14 @@ namespace electionguard::facades
             return add(reinterpret_cast<uint32_t *>(a), reinterpret_cast<uint32_t *>(b),
                        reinterpret_cast<uint32_t *>(res));
         }
-        // return hacl::Bignum4096::add(a, b, res);
+        return hacl::Bignum4096::add(a, b, res);
     }
 
     // Sub
     uint32_t Bignum4096::sub(uint32_t *a, uint32_t *b, uint32_t *res)
     {
 
-        // return hacl::Bignum4096_32::sub(a, b, res);
+        return hacl::Bignum4096_32::sub(a, b, res);
     }
     uint64_t Bignum4096::sub(uint64_t *a, uint64_t *b, uint64_t *res)
     {
@@ -83,13 +80,13 @@ namespace electionguard::facades
             return sub(reinterpret_cast<uint32_t *>(a), reinterpret_cast<uint32_t *>(b),
                        reinterpret_cast<uint32_t *>(res));
         }
-        //  return hacl::Bignum4096::sub(a, b, res);
+        return hacl::Bignum4096::sub(a, b, res);
     }
 
     // Mul
     void Bignum4096::mul(uint32_t *a, uint32_t *b, uint32_t *res)
     {
-        //  hacl::Bignum4096_32::mul(a, b, res);
+        hacl::Bignum4096_32::mul(a, b, res);
     }
     void Bignum4096::mul(uint64_t *a, uint64_t *b, uint64_t *res)
     {
@@ -97,13 +94,13 @@ namespace electionguard::facades
             return mul(reinterpret_cast<uint32_t *>(a), reinterpret_cast<uint32_t *>(b),
                        reinterpret_cast<uint32_t *>(res));
         }
-        //  hacl::Bignum4096::mul(a, b, res);
+        hacl::Bignum4096::mul(a, b, res);
     }
 
     // Mod
     bool Bignum4096::mod(uint32_t *n, uint32_t *a, uint32_t *res)
     {
-        // return hacl::Bignum4096_32::mod(n, a, res);
+        return hacl::Bignum4096_32::mod(n, a, res);
     }
     bool Bignum4096::mod(uint64_t *n, uint64_t *a, uint64_t *res)
     {
@@ -111,14 +108,14 @@ namespace electionguard::facades
             return mod(reinterpret_cast<uint32_t *>(n), reinterpret_cast<uint32_t *>(a),
                        reinterpret_cast<uint32_t *>(res));
         }
-        //  return hacl::Bignum4096::mod(n, a, res);
+        return hacl::Bignum4096::mod(n, a, res);
     }
 
     // ModExp
     bool Bignum4096::modExp(uint32_t *n, uint32_t *a, uint32_t bBits, uint32_t *b, uint32_t *res,
                             bool useConstTime /* = true */)
     {
-        // return hacl::Bignum4096_32::modExp(n, a, bBits, b, res, useConstTime);
+        return hacl::Bignum4096_32::modExp(n, a, bBits, b, res, useConstTime);
     }
     bool Bignum4096::modExp(uint64_t *n, uint64_t *a, uint32_t bBits, uint64_t *b, uint64_t *res,
                             bool useConstTime /* = true */)
@@ -127,13 +124,13 @@ namespace electionguard::facades
             return modExp(reinterpret_cast<uint32_t *>(n), reinterpret_cast<uint32_t *>(a), bBits,
                           reinterpret_cast<uint32_t *>(b), reinterpret_cast<uint32_t *>(res));
         }
-        //  return hacl::Bignum4096::modExp(n, a, bBits, b, res, useConstTime);
+        return hacl::Bignum4096::modExp(n, a, bBits, b, res, useConstTime);
     }
 
     // Mod Inv
     bool Bignum4096::modInvPrime(uint32_t *n, uint32_t *a, uint32_t *res)
     {
-        // return hacl::Bignum4096_32::modInvPrime(n, a, res);
+        return hacl::Bignum4096_32::modInvPrime(n, a, res);
     }
 
     bool Bignum4096::modInvPrime(uint64_t *n, uint64_t *a, uint64_t *res)
@@ -142,26 +139,26 @@ namespace electionguard::facades
             return modInvPrime(reinterpret_cast<uint32_t *>(n), reinterpret_cast<uint32_t *>(a),
                                reinterpret_cast<uint32_t *>(res));
         }
-        // return hacl::Bignum4096::modInvPrime(n, a, res);
+        return hacl::Bignum4096::modInvPrime(n, a, res);
     }
 
     // FromBytes
     uint32_t *Bignum4096::fromBytes32(uint32_t len, uint8_t *bytes)
     {
-        // return hacl::Bignum4096_32::fromBytes(len, bytes);
+        return hacl::Bignum4096_32::fromBytes(len, bytes);
     }
     uint64_t *Bignum4096::fromBytes64(uint32_t len, uint8_t *bytes)
     {
         if (use32BitMath) {
             return reinterpret_cast<uint64_t *>(fromBytes32(len, bytes));
         }
-        //  return hacl::Bignum4096::fromBytes(len, bytes);
+        return hacl::Bignum4096::fromBytes(len, bytes);
     }
 
     // ToBytes
     void Bignum4096::toBytes(uint32_t *bytes, uint8_t *res)
     {
-        // return hacl::Bignum4096_32::toBytes(bytes, res);
+        return hacl::Bignum4096_32::toBytes(bytes, res);
     }
     void Bignum4096::toBytes(uint64_t *bytes, uint8_t *res)
     {
@@ -169,17 +166,17 @@ namespace electionguard::facades
             toBytes(reinterpret_cast<uint32_t *>(bytes), res);
             return;
         }
-        //  return hacl::Bignum4096::toBytes(bytes, res);
+        return hacl::Bignum4096::toBytes(bytes, res);
     }
 
     // LessThan
     uint32_t Bignum4096::lessThan(uint32_t *a, uint32_t *b)
     {
-        // return hacl::Bignum4096_32::lessThan(a, b);
+        return hacl::Bignum4096_32::lessThan(a, b);
     }
     uint64_t Bignum4096::lessThan(uint64_t *a, uint64_t *b)
     {
-        //  return hacl::Bignum4096::lessThan(a, b);
+        return hacl::Bignum4096::lessThan(a, b);
     }
 
 #pragma endregion
@@ -237,34 +234,34 @@ namespace electionguard::facades
 
     void Bignum4096::mod(uint32_t *a, uint32_t *res) const
     {
-        // TODO: this one wa working
-        // auto &guy = pimpl->element;
-        // std::get<unique_ptr<hacl::Bignum4096_32>>(guy)->mod(a, res);
+        std::get<unique_ptr<hacl::Bignum4096_32>>(pimpl->element)->mod(a, res);
     }
     void Bignum4096::mod(uint64_t *a, uint64_t *res) const
     {
-        // if (pimpl->prefer32BitMath) {
-        //     mod(reinterpret_cast<uint32_t *>(a), reinterpret_cast<uint32_t *>(res));
-        //     return;
-        // }
-        // std::get<hacl::Bignum4096>(pimpl->element).mod(a, res);
+        if (pimpl->prefer32BitMath) {
+            mod(reinterpret_cast<uint32_t *>(a), reinterpret_cast<uint32_t *>(res));
+            return;
+        }
+        std::get<unique_ptr<hacl::Bignum4096>>(pimpl->element)->mod(a, res);
     }
 
     void Bignum4096::modExp(uint32_t *a, uint32_t bBits, uint32_t *b, uint32_t *res,
                             bool useConstTime /* = true */) const
     {
-        //  std::get<hacl::Bignum4096_32>(pimpl->element).modExp(a, bBits, b, res, useConstTime);
+        std::get<unique_ptr<hacl::Bignum4096_32>>(pimpl->element)
+          ->modExp(a, bBits, b, res, useConstTime);
     }
     void Bignum4096::modExp(uint64_t *a, uint32_t bBits, uint64_t *b, uint64_t *res,
                             bool useConstTime /* = true */) const
     {
-        // if (pimpl->prefer32BitMath) {
-        //     modExp(reinterpret_cast<uint32_t *>(a), bBits, reinterpret_cast<uint32_t *>(b),
-        //            reinterpret_cast<uint32_t *>(res), useConstTime);
-        //     return;
-        // }
+        if (pimpl->prefer32BitMath) {
+            modExp(reinterpret_cast<uint32_t *>(a), bBits, reinterpret_cast<uint32_t *>(b),
+                   reinterpret_cast<uint32_t *>(res), useConstTime);
+            return;
+        }
 
-        // std::get<hacl::Bignum4096>(pimpl->element).modExp(a, bBits, b, res, useConstTime);
+        std::get<unique_ptr<hacl::Bignum4096>>(pimpl->element)
+          ->modExp(a, bBits, b, res, useConstTime);
     }
 
     // ModInv
@@ -273,43 +270,44 @@ namespace electionguard::facades
 
     void Bignum4096::to_montgomery_form(uint32_t *a, uint32_t *aM) const
     {
-        //  std::get<hacl::Bignum4096_32>(pimpl->element).to_montgomery_form(a, aM);
+        std::get<unique_ptr<hacl::Bignum4096_32>>(pimpl->element)->to_montgomery_form(a, aM);
     }
     void Bignum4096::to_montgomery_form(uint64_t *a, uint64_t *aM) const
     {
-        // if (pimpl->prefer32BitMath) {
-        //     to_montgomery_form(reinterpret_cast<uint32_t *>(a), reinterpret_cast<uint32_t *>(aM));
-        // }
-        // std::get<hacl::Bignum4096>(pimpl->element).to_montgomery_form(a, aM);
+        if (pimpl->prefer32BitMath) {
+            to_montgomery_form(reinterpret_cast<uint32_t *>(a), reinterpret_cast<uint32_t *>(aM));
+        }
+        std::get<unique_ptr<hacl::Bignum4096>>(pimpl->element)->to_montgomery_form(a, aM);
     }
 
     void Bignum4096::from_montgomery_form(uint32_t *aM, uint32_t *a) const
     {
-        //  std::get<hacl::Bignum4096_32>(pimpl->element).from_montgomery_form(aM, a);
+        std::get<unique_ptr<hacl::Bignum4096_32>>(pimpl->element)->from_montgomery_form(aM, a);
     }
     void Bignum4096::from_montgomery_form(uint64_t *aM, uint64_t *a) const
     {
-        // if (pimpl->prefer32BitMath) {
-        //     from_montgomery_form(reinterpret_cast<uint32_t *>(aM), reinterpret_cast<uint32_t *>(a));
-        // }
-        // std::get<hacl::Bignum4096>(pimpl->element).from_montgomery_form(aM, a);
+        if (pimpl->prefer32BitMath) {
+            from_montgomery_form(reinterpret_cast<uint32_t *>(aM), reinterpret_cast<uint32_t *>(a));
+        }
+        std::get<unique_ptr<hacl::Bignum4096>>(pimpl->element)->from_montgomery_form(aM, a);
     }
 
     void Bignum4096::montgomery_mod_mul_stay_in_mont_form(uint32_t *aM, uint32_t *bM,
                                                           uint32_t *cM) const
     {
-        // std::get<hacl::Bignum4096_32>(pimpl->element)
-        //   .montgomery_mod_mul_stay_in_mont_form(aM, bM, cM);
+        std::get<unique_ptr<hacl::Bignum4096_32>>(pimpl->element)
+          ->montgomery_mod_mul_stay_in_mont_form(aM, bM, cM);
     }
     void Bignum4096::montgomery_mod_mul_stay_in_mont_form(uint64_t *aM, uint64_t *bM,
                                                           uint64_t *cM) const
     {
-        // if (pimpl->prefer32BitMath) {
-        //     montgomery_mod_mul_stay_in_mont_form(reinterpret_cast<uint32_t *>(aM),
-        //                                          reinterpret_cast<uint32_t *>(bM),
-        //                                          reinterpret_cast<uint32_t *>(cM));
-        // }
-        // std::get<hacl::Bignum4096>(pimpl->element).montgomery_mod_mul_stay_in_mont_form(aM, bM, cM);
+        if (pimpl->prefer32BitMath) {
+            montgomery_mod_mul_stay_in_mont_form(reinterpret_cast<uint32_t *>(aM),
+                                                 reinterpret_cast<uint32_t *>(bM),
+                                                 reinterpret_cast<uint32_t *>(cM));
+        }
+        std::get<unique_ptr<hacl::Bignum4096>>(pimpl->element)
+          ->montgomery_mod_mul_stay_in_mont_form(aM, bM, cM);
     }
 
 #pragma endregion
