77cd42e596cc2912ec1325d36b0c05fb08bb14f1
SteveMaier-IRT <steve.maier@infernored.com>
2023-03-03 09:51:48 -0500

    Moved the interfaces into new folder Created new types for the election setup Created new services for those types.

---
+++ src/electionguard-ui/ElectionGuard.UI.Lib/Models/ConstantsRecord.cs
@@ -0,0 +1,15 @@
+﻿namespace ElectionGuard.UI.Lib.Models;
+
+public partial class ConstantsRecord : DatabaseRecord
+{
+    [ObservableProperty]
+    private string? _electionId;
+
+    [ObservableProperty]
+    private string? _constantsData;
+
+    public ConstantsRecord() : base(nameof(ConstantsRecord))
+    {
+    }
+
+}
