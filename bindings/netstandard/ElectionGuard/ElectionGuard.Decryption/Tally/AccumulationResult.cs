20e50399e4da4c3c8662cf0adac1ec337d7e6c3c
AddressXception <github@addressxception.com>
2023-03-23 17:02:27 -0400

    Annotate Accumulation Result

--- bindings/netstandard/ElectionGuard/ElectionGuard.Decryption/Tally/AccumulationResult.cs
+++ bindings/netstandard/ElectionGuard/ElectionGuard.Decryption/Tally/AccumulationResult.cs
@@ -1,11 +1,26 @@
+using System.Text;
 using ElectionGuard.Encryption.Ballot;
 
 namespace ElectionGuard.Decryption.Tally;
 
+/// <summary>
+/// The result of a ciphertext tally accumulation operation.
+/// </summary>
 public class AccumulationResult
 {
+    /// <summary>
+    /// The id of the tally
+    /// </summary>
     public string TallyId { get; init; }
+
+    /// <summary>
+    /// The set of ballot ids that were successfully accumulated
+    /// </summary>
     public HashSet<string> Accumulated { get; init; }
+
+    /// <summary>
+    /// The set of ballot ids that failed to accumulate along with the reason
+    /// </summary>
     public Dictionary<string, BallotValidationResult> Failed { get; init; }
 
     public AccumulationResult(string tallyId)
@@ -79,12 +94,39 @@ public class AccumulationResult
         };
     }
 
-    public void Add(AccumulationResult other)
+    /// <summary>
+    /// Add the results of another accumulation operation to this one
+    /// </summary>
+    public AccumulationResult Add(AccumulationResult other)
     {
+        // check the other accumulated ballots and verify there are no duplicates
+        foreach (var ballotId in other.Accumulated)
+        {
+            if (Accumulated.Contains(ballotId))
+            {
+                throw new ArgumentException($"Ballot {ballotId} already added to tally {TallyId}");
+            }
+        }
+
         Accumulated.UnionWith(other.Accumulated);
         foreach (var (ballotId, validationResult) in other.Failed)
         {
             Failed.Add(ballotId, validationResult);
         }
+
+        return this;
+    }
+
+    public override string ToString()
+    {
+        var builder = new StringBuilder();
+        _ = builder.AppendLine($"Tally {TallyId}");
+        _ = builder.AppendLine($"Accumulated {Accumulated.Count} ballots");
+        _ = builder.AppendLine($"Failed {Failed.Count} ballots");
+        foreach (var (ballotId, validationResult) in Failed)
+        {
+            _ = builder.AppendLine($"- Ballot {ballotId}: {validationResult}");
+        }
+        return builder.ToString();
     }
 }
