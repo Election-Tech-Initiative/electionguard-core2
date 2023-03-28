6ee72ace091f837589635b1d4b9836e8f73a4cb6
SteveMaier-IRT <steve.maier@infernored.com>
2023-03-28 11:05:04 -0400

    Add all of the rest of the tally database code

---
+++ src/electionguard-ui/ElectionGuard.UI.Lib/Models/PlaintextTallyRecord.cs
@@ -0,0 +1,14 @@
+﻿namespace ElectionGuard.UI.Lib.Models;
+
+public partial class PlaintextTallyRecord : DatabaseRecord
+{
+    [ObservableProperty]
+    private string _tallyId = string.Empty;
+
+    [ObservableProperty]
+    private string? _plaintextTallyData;
+
+    public PlaintextTallyRecord() : base(nameof(PlaintextTallyRecord))
+    {
+    }
+}
---
+++ src/electionguard-ui/ElectionGuard.UI.Lib/Services/PlaintextTallyService.cs
@@ -0,0 +1,30 @@
+﻿using ElectionGuard.UI.Lib.Models;
+using MongoDB.Driver;
+
+namespace ElectionGuard.UI.Lib.Services;
+
+/// <summary>
+/// Data Service for Tallies
+/// </summary>
+public class PlainTallyService : BaseDatabaseService<PlaintextTallyRecord>
+{
+    /// <summary>
+    /// The collection name to use to get/save data into
+    /// </summary>
+    private readonly static string _collection = Constants.TableTallies;
+
+    /// <summary>
+    /// Default constructor that sets the collection name
+    /// </summary>
+    public PlainTallyService() : base(_collection) { }
+
+    /// <summary>
+    /// Gets plain text tally for an election
+    /// </summary>
+    /// <param name="tallyId">tally id to search for</param>
+    public async Task<PlaintextTallyRecord?> GetByTallyIdAsync(string tallyId)
+    {
+        return await GetByFieldAsync(Constants.TallyId, tallyId);
+    }
+
+}
