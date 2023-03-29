0df9469b49fb1ee269c7a50b38f0f161884a6ea5
Steve Maier <82616727+SteveMaier-IRT@users.noreply.github.com>
2023-03-29 13:21:08 -0400

    Setup Tally Db calls (#174)
    * Add all of the rest of the tally database code
    
    * Updating version of Windows SDK

---
+++ src/electionguard-ui/ElectionGuard.UI.Lib/Models/CiphertextTallyRecord.cs
@@ -0,0 +1,14 @@
+﻿namespace ElectionGuard.UI.Lib.Models;
+
+public partial class CiphertextTallyRecord : DatabaseRecord
+{
+    [ObservableProperty]
+    private string _tallyId = string.Empty;
+
+    [ObservableProperty]
+    private string? _ciphertextTallyData;
+
+    public CiphertextTallyRecord() : base(nameof(CiphertextTallyRecord))
+    {
+    }
+}
--- src/electionguard-ui/ElectionGuard.UI.Lib/Services/ElectionService.cs
+++ src/electionguard-ui/ElectionGuard.UI.Lib/Services/ElectionService.cs
@@ -10,7 +10,7 @@ public class ElectionService : BaseDatabaseService<Election>
     /// <summary>
     /// The collection name to use to get/save data into
     /// </summary>
-    private readonly static string _collection = "elections";
+    private readonly static string _collection = Constants.TableElections;
 
     /// <summary>
     /// Default constructor that sets the collection name
