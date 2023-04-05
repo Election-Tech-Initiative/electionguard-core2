f2becae86fe86a6895b4e5c2b116bcad2da52b18
AddressXception <github@addressxception.com>
2023-04-05 10:55:43 -0400

    move the election joint key into the core encryption library
    note this is an incremental commit and it may not compile

---
+++ bindings/netstandard/ElectionGuard/ElectionGuard.Encryption/ElectionJointKey.cs
@@ -0,0 +1,28 @@
+﻿namespace ElectionGuard
+{
+    /// <summary>
+    /// The Election joint key
+    /// </summary>
+    public class ElectionJointKey : DisposableBase
+    {
+        /// <summary>
+        /// The product of the guardian public keys
+        /// K = ∏ ni=1 Ki mod p.
+        /// </summary>
+        public ElementModP JointPublicKey { get; set; }
+
+        /// <summary>
+        /// The hash of the commitments that the guardians make to each other
+        /// H = H(K 1,0 , K 2,0 ... , K n,0 )
+        /// </summary>
+        public ElementModQ CommitmentHash { get; set; }
+
+        protected override void DisposeUnmanaged()
+        {
+            base.DisposeUnmanaged();
+
+            JointPublicKey?.Dispose();
+            CommitmentHash?.Dispose();
+        }
+    }
+}
