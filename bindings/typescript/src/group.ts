df4a692781bbad31fd4dc834995149ba55b3e5b4
AddressXception <github@addressxception.com>
2023-04-12 17:21:53 -0400

    add group tests

--- bindings/typescript/src/group.ts
+++ bindings/typescript/src/group.ts
@@ -1,10 +1,43 @@
+import { ElementModP, ElementModQ } from "./wasm/electionguard";
 import Module from "./wasm/electionguard.wasm";
 
 // TODO: wrap the library in a promise
 
-export function addModQ(a: number, b: number): BigInt {
-  var elem1 = Module.ElementModQ.fromUint64(BigInt(a), false);
-  var elem2 = Module.ElementModQ.fromUint64(BigInt(b), false);
-  var result = Module.GroupFunctions.addModQ(elem1, elem2);
-  return BigInt("0x" + result.toHex());
+export class ElementModPConverter {
+  static fromNumber(a: number): ElementModP {
+    var result = ElementModPConverter.fromBigInt(BigInt(a));
+    return result;
+  }
+  static fromBigInt(a: BigInt): ElementModP {
+    var result = Module.ElementModP.fromUint64(a, false);
+    return result;
+  }
+  static fromHex(a: string): ElementModP {
+    var result = Module.ElementModP.fromHex(a, false);
+    return result;
+  }
+}
+
+export class ElementModQConverter {
+  static fromNumber(a: number): ElementModQ {
+    var result = ElementModQConverter.fromBigInt(BigInt(a));
+    return result;
+  }
+  static fromBigInt(a: BigInt): ElementModQ {
+    var result = Module.ElementModQ.fromUint64(a, false);
+    return result;
+  }
+  static fromHex(a: string): ElementModQ {
+    var result = Module.ElementModQ.fromHex(a, false);
+    return result;
+  }
+}
+
+export class GroupFunctions {
+  static addModQ(a: number, b: number): BigInt {
+    var elem1 = ElementModQConverter.fromNumber(a);
+    var elem2 = ElementModQConverter.fromNumber(b);
+    var result = Module.GroupFunctions.addModQ(elem1, elem2);
+    return BigInt("0x" + result.toHex());
+  }
 }
--- bindings/typescript/test/group.test.ts
+++ bindings/typescript/test/group.test.ts
@@ -1,9 +1,27 @@
 import { assert } from "chai";
-import { addModQ } from "../src/group";
+import {
+  ElementModPConverter,
+  ElementModQConverter,
+  GroupFunctions,
+} from "../src/group";
 
-describe("ElementModQ Math Tests", () => {
+describe("ElementModPConverter Tests", () => {
+  it("should convert from a hex string", () => {
+    const result = ElementModPConverter.fromHex("0x05");
+    assert.equal(result.toHex(), "05");
+  });
+});
+
+describe("ElementModQConverter Tests", () => {
+  it("should convert from a hex string", () => {
+    const result = ElementModQConverter.fromHex("0x05");
+    assert.equal(result.toHex(), "05");
+  });
+});
+
+describe("GroupFunctions addModQ Math Tests", () => {
   it("should return 5 when 2 is added to 3", () => {
-    const result = addModQ(2, 3);
+    const result = GroupFunctions.addModQ(2, 3);
     assert.equal(result, 5n);
   });
 });
