ff12f9cad6f6dca9db3af007adea2f2e49e56114
AddressXception <github@addressxception.com>
2023-04-12 16:30:10 -0400

    add ballot tests

--- bindings/typescript/src/ballot.ts
+++ bindings/typescript/src/ballot.ts
@@ -1,9 +1,18 @@
 import Module from "./wasm/electionguard.wasm";
-import { PlaintextBallot } from "./wasm/electionguard";
+import { CiphertextBallot, PlaintextBallot } from "./wasm/electionguard";
 
 // TODO: wrap the library in a promise
 
-export function fromJson(json: string): PlaintextBallot {
-  var result = Module.PlaintextBallot.fromJson(json);
-  return result;
+export class PlaintextBallotConverter {
+  static fromJson(json: string): PlaintextBallot {
+    var result = Module.PlaintextBallot.fromJson(json);
+    return result;
+  }
+}
+
+export class CiphertextBallotConverter {
+  static fromJson(json: string): CiphertextBallot {
+    var result = Module.CiphertextBallot.fromJson(json);
+    return result;
+  }
 }
--- bindings/typescript/test/ballot.test.ts
+++ bindings/typescript/test/ballot.test.ts
@@ -1,11 +1,33 @@
 import { assert } from "chai";
-import ballot from "../../../data/ballot_in_simple.json";
-import { fromJson } from "../src/ballot";
+import simpleBallot from "../../../data/ballot_in_simple.json";
+import test_data from "../../../data/test/test-data.json";
+import {
+  CiphertextBallotConverter,
+  PlaintextBallotConverter,
+} from "../src/ballot";
 
-describe("PlaintextBallot Tests", () => {
-  it("should convert from json", () => {
-    const expected = JSON.stringify(ballot);
-    const result = fromJson(expected);
-    assert.isTrue(result.toJson().includes(ballot.object_id));
+const plaintextBallots = (test_data as unknown as any).plaintext_ballots;
+
+const ciphertextBallots = (test_data as unknown as any).ciphertext_ballots;
+
+describe("PlaintextBallotConverter Tests", () => {
+  it("should convert simple ballot from json", () => {
+    const expected = JSON.stringify(simpleBallot);
+    const result = PlaintextBallotConverter.fromJson(expected);
+    assert.isTrue(result.toJson().includes(simpleBallot.object_id));
+  });
+  it("should convert complex ballot from json", () => {
+    const expected = JSON.stringify(plaintextBallots[0]);
+    const result = PlaintextBallotConverter.fromJson(expected);
+    assert.isTrue(result.toJson().includes(plaintextBallots[0].object_id));
   });
 });
+
+// describe("CiphertextBallotConverter Tests", () => {
+//   it("should convert complex ballot from json", () => {
+//     const expected = JSON.stringify(ciphertextBallots[0]);
+//     console.log(expected);
+//     const result = CiphertextBallotConverter.fromJson(expected);
+//     assert.isTrue(result.toJson().includes(ciphertextBallots[0].object_id));
+//   });
+// });
