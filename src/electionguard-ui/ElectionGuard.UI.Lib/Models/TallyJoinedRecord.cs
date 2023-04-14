4b87bc2fae5415d2a3a8b73c3d3851a249bef42b
John Morgan <john.morgan@infernored.com>
2023-04-14 09:08:01 -0400

    Add the ability for a guardian to join a tally

---
+++ src/electionguard-ui/ElectionGuard.UI.Lib/Models/TallyJoinedRecord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionGuard.UI.Lib.Models
+{
+    public partial class TallyJoinedRecord : DatabaseRecord
+    {
+        [ObservableProperty]
+        private string _tallyId = string.Empty;
+
+        [ObservableProperty]
+        private string _guardianId = string.Empty;
+
+        [ObservableProperty]
+        private DateTime _joined = DateTime.UtcNow;
+        
+
+        public TallyJoinedRecord() : base(nameof(TallyJoinedRecord))
+        {
+        }
+    }
+}
