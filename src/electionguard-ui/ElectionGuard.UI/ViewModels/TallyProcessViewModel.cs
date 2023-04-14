2275de3351739a8c13cc3b0566faec06545300d7
John Morgan <john.morgan@infernored.com>
2023-04-14 11:44:46 -0400

    cleanup usings

--- src/electionguard-ui/ElectionGuard.UI/ViewModels/TallyProcessViewModel.cs
+++ src/electionguard-ui/ElectionGuard.UI/ViewModels/TallyProcessViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ElectionGuard.UI.Lib.Services;
+﻿using CommunityToolkit.Mvvm.Input;
 
 namespace ElectionGuard.UI.ViewModels;
 
