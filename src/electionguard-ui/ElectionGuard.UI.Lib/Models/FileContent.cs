186ce902d6032475786124628b45d0708e334ec3
Steve Maier <82616727+SteveMaier-IRT@users.noreply.github.com>
2023-04-20 08:48:12 -0400

    Task/zip service (#215)

---
+++ src/electionguard-ui/ElectionGuard.UI.Lib/Models/FileContent.cs
@@ -0,0 +1,6 @@
+﻿namespace ElectionGuard.UI.Lib.Models;
+
+/// <summary>
+/// structure to hold data for files to write to zip file
+/// </summary>
+public record FileContents(string FileName, string Contents);
