f9ee4c7928caca624bac040eb7b026ec6b54b336
John Morgan <john.morgan@infernored.com>
2023-04-26 08:51:17 -0400

    Tally processing - guardian

--- src/electionguard-ui/ElectionGuard.UI/ViewModels/TallyProcessViewModel.cs
+++ src/electionguard-ui/ElectionGuard.UI/ViewModels/TallyProcessViewModel.cs
@@ -1,4 +1,5 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
 
 namespace ElectionGuard.UI.ViewModels;
 
@@ -10,8 +11,84 @@ public partial class TallyProcessViewModel : BaseViewModel
     [ObservableProperty]
     private string _tallyId = string.Empty;
 
-    public TallyProcessViewModel(IServiceProvider serviceProvider) :
+    [ObservableProperty]
+    private TallyRecord _tally = new();
+
+    [ObservableProperty]
+    private Election _currentElection = new();
+
+    [ObservableProperty]
+    private List<BallotUpload> _ballotUploads = new();
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(JoinTallyCommand))]
+    private List<TallyJoinedRecord> _joinedGuardians = new();
+
+    private ElectionService _electionService;
+    private TallyService _tallyService;
+    private BallotUploadService _ballotUploadService;
+    private TallyJoinedService _tallyJoinedService;
+
+    public TallyProcessViewModel(
+        IServiceProvider serviceProvider,
+        TallyService tallyService,
+        TallyJoinedService tallyJoinedService,
+        ElectionService electionService,
+        BallotUploadService ballotUploadService) :
         base("TallyProcess", serviceProvider)
+    {
+        _tallyService = tallyService;
+        _electionService = electionService;
+        _tallyJoinedService = tallyJoinedService;
+        _ballotUploadService = ballotUploadService;
+    }
+
+    partial void OnTallyIdChanged(string value)
+    {
+        _ = Shell.Current.CurrentPage.Dispatcher.DispatchAsync(async () =>
+        {
+            var firstValue = value;
+            var Tally = await _tallyService.GetByTallyIdAsync(firstValue);
+
+            if (Tally != null)
+            {
+                JoinedGuardians = await _tallyJoinedService.GetByTallyIdAsync(firstValue);
+                var electionId = Tally.ElectionId ?? string.Empty;
+                var election = await _electionService.GetByElectionIdAsync(electionId);
+                if (election is null)
+                {
+                    throw new ElectionGuardException($"ElectionId {electionId} not found! Tally {Tally.Id}");
+                    // TODO: put up some error message somewhere, over the rainbow.
+                }
+
+                CurrentElection = election;
+
+                BallotUploads = await _ballotUploadService.GetByElectionIdAsync(electionId);
+
+                JoinedGuardians = await _tallyJoinedService.GetByTallyIdAsync(Tally.TallyId);
+            }
+        });
+    }
+
+    [RelayCommand(CanExecute = nameof(CanJoinTally))]
+    private void JoinTally()
+    {
+        var joiner = new TallyJoinedRecord()
+        {
+            TallyId = TallyId,
+            GuardianId = UserName!, // can assume not null, since you need to be signed into 
+        };
+
+        Task.Run(async () => await _tallyJoinedService.JoinTally(joiner));
+        JoinedGuardians.Add(joiner);
+    }
+
+    private bool CanJoinTally() => CurrentUserJoinedAlready() && Tally.State == TallyState.PendingGuardiansJoin;
+
+    private bool CurrentUserJoinedAlready() => JoinedGuardians.SingleOrDefault(g => g.GuardianId == UserName) is object;
+
+    [RelayCommand(CanExecute =nameof(CanJoinTally))]
+    private void RejectTally() 
     {
     }
 
