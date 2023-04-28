6bd81611497eb032605b2ba321f0a38d3a1468cc
John Morgan <john.morgan@infernored.com>
2023-04-28 10:25:02 -0400

    Add constants for filenames

--- src/electionguard-ui/ElectionGuard.UI/Helpers/UISettings.cs
+++ src/electionguard-ui/ElectionGuard.UI/Helpers/UISettings.cs
@@ -3,4 +3,10 @@
 public struct UISettings
 {
     public const int LONG_POLLING_INTERVAL = 5;
+    public static struct EncryptionPackageFilenames
+    {
+        public const string CONSTANTS = "constants.json";
+        public const string CONTEXT = "context.json";
+        public const string MANIFEST = "manifest.json";
+    }
 }
