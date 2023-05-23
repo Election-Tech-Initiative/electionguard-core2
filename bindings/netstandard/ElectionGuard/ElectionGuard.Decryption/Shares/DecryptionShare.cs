0f62b6bf51017c251d9f424c585eb1f644b910ad
John Morgan <john.morgan@infernored.com>
2023-05-22 23:17:16 -0400

    Cleanup DecryptionShare

--- bindings/netstandard/ElectionGuard/ElectionGuard.Decryption/Shares/DecryptionShare.cs
+++ bindings/netstandard/ElectionGuard/ElectionGuard.Decryption/Shares/DecryptionShare.cs
@@ -1,30 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ElectionGuard.ElectionSetup;
+﻿using ElectionGuard.ElectionSetup;
 using ElectionGuard.Extensions;
 
-namespace ElectionGuard.Decryption.Shares
+namespace ElectionGuard.Decryption.Shares;
+
+public record DecryptionShare : DisposableRecordBase
 {
-    public record DecryptionShare : DisposableRecordBase
+    public TallyShare TallyShare { get; set; }
+    public Dictionary<string, BallotShare> BallotShares { get; set; }
+
+    public DecryptionShare(TallyShare tallyShare, Dictionary<string, BallotShare> ballotShares) : base()
     {
-        public TallyShare TallyShare { get; set; }
-        public Dictionary<string, BallotShare> BallotShares { get; set; }
+        TallyShare = tallyShare;
+        BallotShares = ballotShares;
+    }
 
-        public DecryptionShare(TallyShare tallyShare, Dictionary<string, BallotShare> ballotShares) : base()
-        {
-            TallyShare = tallyShare;
-            BallotShares = ballotShares;
-        }
-
-        protected override void DisposeManaged()
-        {
-            base.DisposeManaged();
-            TallyShare.Dispose();
-            BallotShares.Values.Dispose();
-        }
-
+    protected override void DisposeManaged()
+    {
+        base.DisposeManaged();
+        TallyShare.Dispose();
+        BallotShares.Values.Dispose();
     }
 }
