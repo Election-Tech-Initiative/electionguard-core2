3d844d164f3a8fd9cfaacd9e0f970abf1ea37e6e
John Morgan <john.morgan@infernored.com>
2023-05-23 10:35:50 -0400

    Update interface

--- src/electionguard-ui/ElectionGuard.UI/Services/ITallyStateMachine.cs
+++ src/electionguard-ui/ElectionGuard.UI/Services/ITallyStateMachine.cs
@@ -6,6 +6,5 @@ internal interface ITallyStateMachine
     TallyState CurrentState { get; }
     string TallyId { get; set; }
     string ElectionId { get; set; }
-    string UserId { get; set; }
-    Task Run(bool isAdmin = false);
+    Task Run();
 }
