5c6fbfc6530dd9f7f170452cad509e8f93e6a03d
John Morgan <john.morgan@infernored.com>
2023-06-05 12:48:18 -0400

    consistency on converters.

--- src/electionguard-ui/ElectionGuard.UI/Helpers/TallyCanBeAbandonedConverter.cs
+++ src/electionguard-ui/ElectionGuard.UI/Helpers/TallyCanBeAbandonedConverter.cs
@@ -9,10 +9,7 @@ namespace ElectionGuard.UI.Helpers
         public override bool DefaultConvertReturnValue { get; set; }
         public override TallyState DefaultConvertBackReturnValue { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
-        public override TallyState ConvertBackTo(bool value, CultureInfo? culture)
-        {
-            throw new NotImplementedException();
-        }
+        public override TallyState ConvertBackTo(bool value, CultureInfo? culture) => throw new NotImplementedException();
 
         public override bool ConvertFrom(TallyState value, CultureInfo? culture) => value < TallyState.TallyStarted;
     }
