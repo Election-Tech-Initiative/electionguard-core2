b16965cbc27cf8561685c0a57e9130a57b11ba68
SteveMaier-IRT <steve.maier@infernored.com>
2023-06-05 17:37:21 -0400

    Added fix for key ceremony picker.

--- src/electionguard-ui/ElectionGuard.UI/Views/CreateElectionAdminPage.xaml.cs
+++ src/electionguard-ui/ElectionGuard.UI/Views/CreateElectionAdminPage.xaml.cs
@@ -2,14 +2,13 @@
 
 public partial class CreateElectionAdminPage
 {
-	public CreateElectionAdminPage(CreateElectionViewModel vm) : base(vm)
-	{
-		InitializeComponent();
-	}
+    public CreateElectionAdminPage(CreateElectionViewModel vm) : base(vm)
+    {
+        InitializeComponent();
+    }
 
     private void Picker_SelectedIndexChanged(object sender, EventArgs e)
     {
-//		var picker = (Picker)sender;
-//		picker.TextColor = Colors.Black;
+        keyEntry.Text = ((KeyCeremonyRecord)keyPicker.SelectedItem).Name;
     }
 }
