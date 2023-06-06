5bde342753069773269c6f043601d35ec121ef9f
John Morgan <70619927+john-s-morgan@users.noreply.github.com>
2023-06-06 13:17:25 -0400

    update namespacing for converters (#306)

---
+++ src/electionguard-ui/ElectionGuard.UI/Converters/BackupConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace ElectionGuard.UI.Converters;
+
+internal class BackupConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (bool)value ? "backup.png" : "notbackup.png";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
