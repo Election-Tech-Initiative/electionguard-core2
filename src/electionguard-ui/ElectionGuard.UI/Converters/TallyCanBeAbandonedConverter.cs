1a91d543d33cb3174ce473bbac7c6a2a21eebdb0
John Morgan <john.morgan@infernored.com>
2023-06-07 13:33:02 -0400

    Fix converters

--- src/electionguard-ui/ElectionGuard.UI/Converters/TallyCanBeAbandonedConverter.cs
+++ src/electionguard-ui/ElectionGuard.UI/Converters/TallyCanBeAbandonedConverter.cs
@@ -4,15 +4,14 @@ using CommunityToolkit.Maui.Converters;
 namespace ElectionGuard.UI.Converters;
 
 internal class TallyCanBeAbandonedConverter
-    : BaseConverter<TallyState, bool,bool>
+    : BaseConverter<TallyState, bool>
 {
     public override bool DefaultConvertReturnValue { get; set; }
     public override TallyState DefaultConvertBackReturnValue { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
-    public override TallyState ConvertBackTo(bool value, bool parameter, CultureInfo? culture)
+    public override TallyState ConvertBackTo(bool value, CultureInfo? culture)
     {
         throw new NotImplementedException();
     }
-
-    public override bool ConvertFrom(TallyState value, bool isAdmin,  CultureInfo? culture) => isAdmin && value < TallyState.TallyStarted;
+    public override bool ConvertFrom(TallyState value, CultureInfo? culture) => value < TallyState.TallyStarted;
 }
--- src/electionguard-ui/ElectionGuard.UI/Converters/TallyNotStartedConverter.cs
+++ src/electionguard-ui/ElectionGuard.UI/Converters/TallyNotStartedConverter.cs
@@ -10,5 +10,5 @@ internal class TallyNotStartedConverter
     public override TallyState DefaultConvertBackReturnValue { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
     public override TallyState ConvertBackTo(bool value, CultureInfo? culture) => throw new NotImplementedException();
-    public override bool ConvertFrom(TallyState value, CultureInfo? culture) => value >= TallyState.TallyStarted;
+    public override bool ConvertFrom(TallyState value, CultureInfo? culture) => value < TallyState.TallyStarted;
 }
