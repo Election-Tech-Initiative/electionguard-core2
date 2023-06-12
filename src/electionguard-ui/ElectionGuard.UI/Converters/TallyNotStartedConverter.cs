9aa6605494d1c5c4e660429c8394cd40da203384
John Morgan <john.morgan@infernored.com>
2023-06-12 15:29:36 -0400

    Add converter for TallyComplete

--- src/electionguard-ui/ElectionGuard.UI/Converters/TallyNotStartedConverter.cs
+++ src/electionguard-ui/ElectionGuard.UI/Converters/TallyNotStartedConverter.cs
@@ -3,12 +3,10 @@ using CommunityToolkit.Maui.Converters;
 
 namespace ElectionGuard.UI.Converters;
 
-internal class TallyNotStartedConverter
-    : BaseConverter<TallyState, bool>
+public class TallyNotStartedConverter
+    : TallyStateConverter
 {
-    public override bool DefaultConvertReturnValue { get; set; }
-    public override TallyState DefaultConvertBackReturnValue { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-    public override TallyState ConvertBackTo(bool value, CultureInfo? culture) => throw new NotImplementedException();
-    public override bool ConvertFrom(TallyState value, CultureInfo? culture) => value < TallyState.TallyStarted;
+    public TallyNotStartedConverter() : base(TallyState.TallyStarted)
+    {
+    }
 }
