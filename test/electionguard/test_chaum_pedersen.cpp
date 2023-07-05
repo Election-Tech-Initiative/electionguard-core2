494b01acfdb7d5bde8e343b28e11363f073b9efd
AddressXception <github@addressxception.com>
2023-07-05 15:37:09 -0400

    break out cp proof tests

--- test/electionguard/test_chaum_pedersen.cpp
+++ test/electionguard/test_chaum_pedersen.cpp
@@ -87,38 +87,6 @@ TEST_CASE("Disjunctive CP Proof simple valid inputs generate valid proofs")
           true);
 }
 
-TEST_CASE("Constant CP Proof encryption of zero")
-{
-    const auto &nonce = ONE_MOD_Q();
-    const auto &seed = TWO_MOD_Q();
-    auto keypair = ElGamalKeyPair::fromSecret(TWO_MOD_Q(), false);
-
-    auto message = elgamalEncrypt(0UL, nonce, *keypair->getPublicKey());
-    auto proof = ConstantChaumPedersenProof::make(*message, nonce, *keypair->getPublicKey(), seed,
-                                                  ONE_MOD_Q(), 0UL);
-    auto badProof = ConstantChaumPedersenProof::make(*message, nonce, *keypair->getPublicKey(),
-                                                     seed, ONE_MOD_Q(), 1UL);
-
-    CHECK(proof->isValid(*message, *keypair->getPublicKey(), ONE_MOD_Q()) == true);
-    CHECK(badProof->isValid(*message, *keypair->getPublicKey(), ONE_MOD_Q()) == false);
-}
-
-TEST_CASE("Constant CP Proof encryption of one")
-{
-    auto keypair = ElGamalKeyPair::fromSecret(TWO_MOD_Q(), false);
-    const auto &nonce = ONE_MOD_Q();
-    const auto &seed = TWO_MOD_Q();
-
-    auto message = elgamalEncrypt(1UL, nonce, *keypair->getPublicKey());
-    auto proof = ConstantChaumPedersenProof::make(*message, nonce, *keypair->getPublicKey(), seed,
-                                                  ONE_MOD_Q(), 1UL);
-    auto badProof = ConstantChaumPedersenProof::make(*message, nonce, *keypair->getPublicKey(),
-                                                     seed, ONE_MOD_Q(), 0UL);
-
-    CHECK(proof->isValid(*message, *keypair->getPublicKey(), ONE_MOD_Q()) == true);
-    CHECK(badProof->isValid(*message, *keypair->getPublicKey(), ONE_MOD_Q()) == false);
-}
-
 TEST_CASE("Disjunctive CP Proof encryption of zero with precomputed values succeeds")
 {
     const auto &nonce = ONE_MOD_Q();
@@ -180,30 +148,81 @@ TEST_CASE("Disjunctive CP Proof encryption of one with precomputed values succee
     const auto &seed = TWO_MOD_Q();
 
     // cause a two triples and a quad to be populated
-    PrecomputeBufferContext::initialize(*keypair->getPublicKey(), 2);
+    PrecomputeBufferContext::initialize(*keypair->getPublicKey(), 1);
     PrecomputeBufferContext::start();
     PrecomputeBufferContext::stop();
 
     // this function runs off to look in the precomputed values buffer and if
     // it finds what it needs the the returned class will contain those values
     auto precomputedValues1 = PrecomputeBufferContext::getPrecomputedSelection();
-    auto precomputedValues2 = PrecomputeBufferContext::getPrecomputedSelection();
 
     CHECK(precomputedValues1 != nullptr);
-    CHECK(precomputedValues2 != nullptr);
 
     auto message1 =
       elgamalEncrypt(1UL, *keypair->getPublicKey(), *precomputedValues1->getPartialEncryption());
 
-    auto message2 =
-      elgamalEncrypt(1UL, *keypair->getPublicKey(), *precomputedValues2->getPartialEncryption());
-
     auto proof = DisjunctiveChaumPedersenProof::make(*message1, *precomputedValues1,
                                                      *keypair->getPublicKey(), ONE_MOD_Q(), 1UL);
-    auto badProof = DisjunctiveChaumPedersenProof::make(*message2, *precomputedValues2,
-                                                        *keypair->getPublicKey(), ONE_MOD_Q(), 0UL);
 
     CHECK(proof->isValid(*message1, *keypair->getPublicKey(), ONE_MOD_Q()) == true);
-    CHECK(badProof->isValid(*message2, *keypair->getPublicKey(), ONE_MOD_Q()) == false);
     PrecomputeBufferContext::clear();
+}
+
+TEST_CASE("Disjunctive CP Proof encryption of one with precomputed values invalid proof fails")
+{
+    auto keypair = ElGamalKeyPair::fromSecret(TWO_MOD_Q(), false);
+    const auto &nonce = ONE_MOD_Q();
+    const auto &seed = TWO_MOD_Q();
+
+    // cause a two triples and a quad to be populated
+    PrecomputeBufferContext::initialize(*keypair->getPublicKey(), 1);
+    PrecomputeBufferContext::start();
+    PrecomputeBufferContext::stop();
+
+    // this function runs off to look in the precomputed values buffer and if
+    // it finds what it needs the the returned class will contain those values
+    auto precomputedValues1 = PrecomputeBufferContext::getPrecomputedSelection();
+
+    CHECK(precomputedValues1 != nullptr);
+
+    auto message1 =
+      elgamalEncrypt(1UL, *keypair->getPublicKey(), *precomputedValues1->getPartialEncryption());
+
+    auto badProof = DisjunctiveChaumPedersenProof::make(*message1, *precomputedValues1,
+                                                        *keypair->getPublicKey(), ONE_MOD_Q(), 0UL);
+
+    CHECK(badProof->isValid(*message1, *keypair->getPublicKey(), ONE_MOD_Q()) == false);
+    PrecomputeBufferContext::clear();
+}
+
+TEST_CASE("Constant CP Proof encryption of zero")
+{
+    const auto &nonce = ONE_MOD_Q();
+    const auto &seed = TWO_MOD_Q();
+    auto keypair = ElGamalKeyPair::fromSecret(TWO_MOD_Q(), false);
+
+    auto message = elgamalEncrypt(0UL, nonce, *keypair->getPublicKey());
+    auto proof = ConstantChaumPedersenProof::make(*message, nonce, *keypair->getPublicKey(), seed,
+                                                  ONE_MOD_Q(), 0UL);
+    auto badProof = ConstantChaumPedersenProof::make(*message, nonce, *keypair->getPublicKey(),
+                                                     seed, ONE_MOD_Q(), 1UL);
+
+    CHECK(proof->isValid(*message, *keypair->getPublicKey(), ONE_MOD_Q()) == true);
+    CHECK(badProof->isValid(*message, *keypair->getPublicKey(), ONE_MOD_Q()) == false);
+}
+
+TEST_CASE("Constant CP Proof encryption of one")
+{
+    auto keypair = ElGamalKeyPair::fromSecret(TWO_MOD_Q(), false);
+    const auto &nonce = ONE_MOD_Q();
+    const auto &seed = TWO_MOD_Q();
+
+    auto message = elgamalEncrypt(1UL, nonce, *keypair->getPublicKey());
+    auto proof = ConstantChaumPedersenProof::make(*message, nonce, *keypair->getPublicKey(), seed,
+                                                  ONE_MOD_Q(), 1UL);
+    auto badProof = ConstantChaumPedersenProof::make(*message, nonce, *keypair->getPublicKey(),
+                                                     seed, ONE_MOD_Q(), 0UL);
+
+    CHECK(proof->isValid(*message, *keypair->getPublicKey(), ONE_MOD_Q()) == true);
+    CHECK(badProof->isValid(*message, *keypair->getPublicKey(), ONE_MOD_Q()) == false);
 }
