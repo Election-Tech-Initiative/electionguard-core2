d9dc437718ccdf8022c1e2c34009482382ae3a65
SteveMaier-IRT <steve.maier@infernored.com>
2023-07-07 10:05:18 -0400

    Added precompute interface back for Hart

--- bindings/netstandard/ElectionGuard/ElectionGuard.Encryption/PrecomputeBufferContext.cs
+++ bindings/netstandard/ElectionGuard/ElectionGuard.Encryption/PrecomputeBufferContext.cs
@@ -3,6 +3,123 @@
 namespace ElectionGuard
 {
     /// <summary>
+    /// Class that controls the precompute process
+    /// </summary>
+    public class Precompute
+    {
+        static readonly int DummyBufferSize = 100;   // set a buffer size that will be > 0 and < the default of 5000 for initialization
+
+        readonly int _initCount = -1;            // initializer for count to make sure we are getting a value back from the C++
+        readonly int _initQueueSize = -2;       // initializer for the queue size to make sure that its different than the count and being set
+
+        AutoResetEvent _waitHandle;
+        PrecomputeBufferContext _context;
+
+        /// <summary>
+        /// Default constructor to initialize buffers for testing
+        /// </summary>
+        public Precompute()
+        {
+        }
+
+
+        private PrecomputeStatus _currentStatus = new PrecomputeStatus
+        {
+            Progress = 0,
+            CurrentQueueSize = 0,
+            CurrentState = PrecomputeState.NotStarted
+        };
+        private int _maxBuffers = DummyBufferSize;
+
+        /// <summary>
+        /// Event handler that will give back progress to the calling code
+        /// </summary>
+        public event StatusEventHandler ProgressEvent;
+
+        /// <summary>
+        /// Event handler that will signal that the entire precompute is completed
+        /// </summary>
+        public event StatusEventHandler CompletedEvent;
+
+        /// <summary>
+        /// Internal method used to call the completed event
+        /// </summary>
+        private void OnCompletedEvent()
+        {
+            CompletedEvent?.Invoke(_currentStatus);
+        }
+
+        /// <summary>
+        /// Internal method used to call the progress event
+        /// </summary>
+        private void OnProgressEvent()
+        {
+            ProgressEvent?.Invoke(_currentStatus);
+        }
+
+        /// <summary>
+        /// Get the current status for the current running process
+        /// </summary>
+        /// <returns><see cref="PrecomputeStatus">PrecomputeStatus</see> with all of the latest information</returns>
+        public PrecomputeStatus GetStatus()
+        {
+            return _context.GetStatus();
+        }
+
+        /// <summary>
+        /// Gets the progress of the precompute
+        /// </summary>
+        /// <param name="count">count of the buffer entries</param>
+        /// <param name="queueSize">max size of the buffer queue</param>
+        public void GetProgress(out int count, out int queueSize)
+        {
+            var status = _context.GetStatus();
+            count = (int)status.CurrentQueueSize;
+            queueSize = (int)status.MaxQueueSize;
+        }
+
+        /// <summary>
+        /// Starts the precompute process by creating a new thread to run the process
+        /// </summary>
+        /// <param name="publicKey">The max exponentiation to be calculated</param>
+        /// <param name="buffers">The maximum number of buffers to precompute</param>
+        public void StartPrecomputeAsync(ElementModP publicKey, int buffers = 0)
+        {
+            _currentStatus.CurrentState = PrecomputeState.Running;
+            _context = new PrecomputeBufferContext(publicKey, buffers);
+            _context.StartPrecomputeAsync();
+        }
+
+        /// <summary>
+        /// Stops the precompute process
+        /// </summary>
+        public void StopPrecompute()
+        {
+            _context.StopPrecompute();
+            SendCompleted();
+        }
+
+
+        private void SendCompleted()
+        {
+            int count = -1;
+            int queueSize = -2;
+            GetProgress(out count, out queueSize);
+            if (count == queueSize)
+                _currentStatus.CurrentState = PrecomputeState.Completed;
+            _currentStatus.Progress = (double)count / queueSize;
+            _currentStatus.CurrentQueueSize = count;
+
+            OnCompletedEvent();
+        }
+
+    }
+
+
+
+
+
+    /// <summary>
     /// Delegate definition to return back the status of the precompute process
     /// </summary>
     /// <returns><see cref="PrecomputeStatus">PrecomputeStatus</see> with all of the latest information</returns>
