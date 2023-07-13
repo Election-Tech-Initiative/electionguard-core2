8b5d30888c67b096ffda1dc3657767d5f4b62330
Steve Maier <82616727+SteveMaier-IRT@users.noreply.github.com>
2023-07-13 18:11:28 -0400

    Task/ev testing (#372)
    * ev testing changes
    
    * Rollback version update
    
    * removed .net 7 building for the ui.
    removed the ui.test project
    fix issue with loading tally
    
    * tally fixes
    
    * version number change
    
    * removing testing of the UI
    
    ---------
    
    Co-authored-by: John Morgan <70619927+john-s-morgan@users.noreply.github.com>

--- src/electionguard-ui/ElectionGuard.UI/ViewModels/TallyProcessViewModel.cs
+++ src/electionguard-ui/ElectionGuard.UI/ViewModels/TallyProcessViewModel.cs
@@ -421,15 +421,31 @@ public partial class TallyProcessViewModel : BaseViewModel
 
         _ = Task.Run(async () =>
         {
-            Tally = await _tallyService.GetByTallyIdAsync(TallyId);
-            if (Tally != null)
+            var localTally = await _tallyService.GetByTallyIdAsync(TallyId);
+            if (localTally != null)
             {
-                await UpdateTallyData();
                 try
                 {
-                    if (await _tallyRunner.Run(Tally))
+                    _ = Shell.Current.CurrentPage.Dispatcher.DispatchAsync(async () =>
+                    {
+                        Tally = localTally;
+                    });
+                    await UpdateTallyData();
+#if DEBUG
+                    ErrorMessage = Tally.State.ToString();
+#endif
+                    if (await _tallyRunner.Run(localTally))
                     {
                         ErrorMessage = string.Empty;
+#if DEBUG
+                        ErrorMessage = $"{Tally.State.ToString()} ran";
+#endif
+                    }
+                    else
+                    {
+#if DEBUG
+                        ErrorMessage = $"{Tally.State.ToString()} waiting";
+#endif
                     }
                 }
                 catch (Exception ex)
