3da6de7a77a38d389e55d22da3f3740b7ba0da24
SteveMaier-IRT <steve.maier@infernored.com>
2023-07-14 16:27:11 -0400

    Added creating plaintext ballots that are then encrypted based on an encryption package to the cli.  added a tally of the plain text ballots so that it can be compared with the system.

---
+++ apps/electionguard-cli/Generate/GenerateCommand.cs
@@ -0,0 +1,110 @@
+﻿using ElectionGuard.Encryption.Utils.Generators;
+using ElectionGuard.Extensions;
+using Newtonsoft.Json;
+
+namespace ElectionGuard.CLI.Generate
+{
+    internal class GenerateCommand
+    {
+        public static Task Execute(GenerateOptions options)
+        {
+            try
+            {
+                var command = new GenerateCommand();
+                return command.ExecuteInternal(options);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        private async Task ExecuteInternal(GenerateOptions options)
+        {
+            options.Validate();
+
+            PlainTally? tally = null;
+            var context = Path.Combine(options.WorkingDir, "context.json");
+            var manifest = Path.Combine(options.WorkingDir, "manifest.json");
+            using var internalManifest = GetInternalManifest(manifest);
+            using var encryptionMediator = GetEncryptionMediator(context, manifest);
+
+            var plaintextBallots = BallotGenerator.GetFakeBallots(
+                internalManifest, Random.Shared, options.BallotCount);
+
+            var plaintextPath = Path.Combine(options.WorkingDir, "plaintext");
+            var encryptedPath = Path.Combine(options.WorkingDir, "encrypted");
+
+            foreach (var ballot in plaintextBallots)
+            {
+                var ciphertext = encryptionMediator.Encrypt(ballot, false);
+                if (Random.Shared.NextSingle() > options.SpoiledPercent / 100.0)
+                {
+                    ciphertext.Cast();
+                }
+                else
+                {
+                    ciphertext.Challenge();
+                }
+                var ballotCode = ciphertext.BallotCode;
+                var ballotData = ciphertext.ToJson();
+                File.WriteAllText(Path.Combine(encryptedPath, $"{ballotCode}.json"), ballotData);
+                var data = ballot.ToJson();
+
+                if (tally == null)
+                {
+                    tally = JsonConvert.DeserializeObject<PlainTally>(data);
+                    tally?.Clear();
+                    tally.object_id = $"Tally for {plaintextBallots.Count} ballots";
+                    tally.style_id = string.Empty;
+                }
+                if (ciphertext.IsCast)
+                {
+                    var b = JsonConvert.DeserializeObject<PlainTally>(data);
+                    tally += b;
+                }
+                if (options.PlaintextOutput)
+                {
+                    File.WriteAllText(Path.Combine(plaintextPath, $"{ballotCode}.json"), data);
+                }
+                Console.WriteLine($"Generated Ballot {ballotCode}.json");
+                ciphertext.Dispose();
+            }
+            var tallyJson = JsonConvert.SerializeObject(tally);
+            File.WriteAllText(Path.Combine(options.WorkingDir, $"tally.json"), tallyJson);
+
+            plaintextBallots.Dispose();
+            encryptionMediator.Dispose();
+
+            Console.WriteLine("Generation Complete");
+        }
+
+        private static EncryptionMediator GetEncryptionMediator(string contextFile, string manifestFile)
+        {
+            using var context = GetContext(contextFile);
+            using var internalManifest = GetInternalManifest(manifestFile);
+            using var device = GetDevice();
+            var encryptionMediator = new EncryptionMediator(internalManifest, context, device);
+            return encryptionMediator;
+        }
+
+        private static EncryptionDevice GetDevice()
+        {
+            return new EncryptionDevice(12345UL, 23456UL, 34567UL, "Location");
+        }
+
+        private static InternalManifest GetInternalManifest(string manifestFile)
+        {
+            var manifestJson = File.ReadAllText(manifestFile);
+            using var manifest = new Manifest(manifestJson);
+            return new InternalManifest(manifest);
+        }
+
+        private static CiphertextElectionContext GetContext(string contextFile)
+        {
+            var contextJson = File.ReadAllText(contextFile);
+            return new CiphertextElectionContext(contextJson);
+        }
+    }
+}
---
+++ apps/electionguard-cli/Generate/PlainTally.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionGuard.CLI.Generate
+{
+
+
+    public class PlainTally
+    {
+        public List<Contest> contests { get; set; } = new();
+        public string object_id { get; set; }
+        public string style_id { get; set; }
+
+        public void Clear()
+        {
+            foreach (Contest contest in contests)
+            {
+                contest.Clear();
+            }
+        }
+        public static PlainTally operator +(PlainTally left, PlainTally right)
+        {
+            foreach (var contest in right.contests)
+            {
+                var l = left.contests.SingleOrDefault(c => c.object_id == contest.object_id);
+                if (l != null)
+                {
+                    l += contest;
+                }
+                else
+                {
+                    left.contests.Add(contest);
+                }
+            }
+            return left;
+        }
+    }
+
+    public class Contest
+    {
+        public List<Ballot_Selections> ballot_selections { get; set; } = new();
+        public string object_id { get; set; }
+
+        public void Clear()
+        {
+            foreach (var selection in ballot_selections)
+            {
+                selection.Clear();
+            }
+        }
+
+        public static Contest operator +(Contest left, Contest right)
+        {
+            foreach (var selection in right.ballot_selections)
+            {
+                var l = left.ballot_selections.SingleOrDefault(b => b.object_id == selection.object_id);
+                if (l != null)
+                {
+                    l += selection;
+                }
+                else
+                {
+                    left.ballot_selections.Add(selection);
+                }
+            }
+            return left;
+        }
+    }
+
+    public class Ballot_Selections
+    {
+        public bool is_placeholder_selection { get; set; }
+        public string object_id { get; set; }
+        public int vote { get; set; }
+        public string write_in { get; set; }
+
+        public void Clear()
+        {
+            vote = 0;
+        }
+
+        public static Ballot_Selections operator +(Ballot_Selections left, Ballot_Selections right)
+        {
+            left.vote += right.vote;
+            return left;
+        }
+    }
+}
